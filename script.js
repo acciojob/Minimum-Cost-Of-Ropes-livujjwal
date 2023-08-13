4e7669aec93bd2c91106411be81a584f1d772b8b
Ujjwal Singh <132872642+livujjwal@users.noreply.github.com>
2023-08-13 18:21:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function calculateMinCost() {
-  //your code here
+  let arr = [4, 2, 7, 6, 9];
+ arr.sort();
+ let result = 0;
+ while( 1 < arr.length) {
+    
+    console.log(arr);
+   let price = arr[0] + arr[1];
+   result += price; 
+   arr.splice(0,2);
+  for(let i = 0; i<arr.length; i++)
+  {
+    if(arr[i]>price ){
+      arr.splice(i,0,price);
+      break;
+    }
+    if(i == arr.length-1)
+    {
+      arr.splice(i+1,0,price);
+      break;
+    }
+  }
+   
+ }
+ return result;
+} 
   
-  
-  
-}  
